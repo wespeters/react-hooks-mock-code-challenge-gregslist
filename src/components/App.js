3e9d3c1b40abc5678f658d0480a2825dac6f7ea0
Wesley Peters <wesleypeters@gmail.com>
2023-06-21 15:52:28 -0600

    add advanced deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
+import ListingForm from "./ListingForm";
 
 function App() {
   const [listings, setListings] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isSorted, setIsSorted] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:6001/listings")
@@ -12,13 +14,37 @@ function App() {
       .then(setListings);
   }, []);
 
-  const filteredListings = listings.filter(listing =>
+  function handleSortClick() {
+    setIsSorted(!isSorted);
+  }
+
+  const sortedListings = [...listings].sort((a, b) => {
+    if (a.location < b.location) {
+      return -1;
+    } else if (a.location > b.location) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  const displayedListings = isSorted ? sortedListings : listings;
+
+  const filteredListings = displayedListings.filter(listing =>
     listing.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  function handleAddListing(newListing) {
+    setListings([newListing, ...listings]);
+  }
 
   return (
     <div className="app">
       <Header setSearchTerm={setSearchTerm} />
+      <button onClick={handleSortClick}>
+        Sort by Location {isSorted ? "⬆️" : "⬇️"}
+      </button>
+      <ListingForm onAddListing={handleAddListing} />
       <ListingsContainer listings={filteredListings} setListings={setListings} />
     </div>
   );
---
+++ src/components/ListingForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function ListingForm({ onAddListing }) {
+  const [formData, setFormData] = useState({
+    description: "",
+    image: "",
+    location: ""
+  });
+
+  function handleFormChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:6001/listings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(listingData => onAddListing(listingData));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="description"
+        placeholder="Description"
+        value={formData.description}
+        onChange={handleFormChange}
+      />
+      <input
+        type="text"
+        name="image"
+        placeholder="Image URL"
+        value={formData.image}
+        onChange={handleFormChange}
+      />
+      <input
+        type="text"
+        name="location"
+        placeholder="Location"
+        value={formData.location}
+        onChange={handleFormChange}
+      />
+      <button type="submit">Add Listing</button>
+    </form>
+  );
+}
+
+export default ListingForm;
